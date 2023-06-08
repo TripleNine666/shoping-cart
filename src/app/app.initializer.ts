f774108095d1ce277d390848af0572c387119756
TripleNine666 <kolumb1709@gmail.com>
2023-06-08 15:51:44 +0300

    i18n update

--- src/app/app.initializer.ts
+++ src/app/app.initializer.ts
@@ -2,7 +2,7 @@ import { TranslateService } from '@ngx-translate/core';
 
 export function appInitializerFactory(translate: TranslateService) {
   return () => new Promise(resolve => {
-    translate.use('ru').subscribe(() => {
+    translate.use('en').subscribe(() => {
       resolve(null);
     });
   });
