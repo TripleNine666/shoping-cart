37d20f21631c45168108f7edc1ece63bb5e3a2da
TripleNine666 <kolumb1709@gmail.com>
2023-06-12 10:39:39 +0300

    change language feature

--- src/app/app.initializer.ts
+++ src/app/app.initializer.ts
@@ -2,8 +2,15 @@ import { TranslateService } from '@ngx-translate/core';
 
 export function appInitializerFactory(translate: TranslateService) {
   return () => new Promise(resolve => {
-    translate.use('en').subscribe(() => {
-      resolve(null);
-    });
+    const savedLang = localStorage.getItem('lang');
+    if (savedLang) {
+      translate.use(savedLang).subscribe(() => {
+        resolve(null);
+      })
+    } else {
+      translate.use('en').subscribe(() => {
+        resolve(null);
+      })
+    }
   });
 }
--- src/app/components/UI/menu/menu.component.ts
+++ src/app/components/UI/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MenuItem} from "primeng/api";
 import { AuthService } from "../../../services/auth.service";
 import {TranslateService} from "@ngx-translate/core";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -13,24 +14,31 @@ export class MenuComponent implements OnInit{
   }
 
   items: MenuItem[] = [];
+  subscriptions: Subscription[] = [];
 
   ngOnInit() {
-
     this.items = [
       {
-        label: this.translate.instant('UI.menu.orders-history'),
+        label: 'UI.menu.orders-history',
         icon: 'pi pi-history',
         routerLink: '/order-history'
       },
       {
-        label: this.translate.instant('UI.menu.contacts'),
+        label: 'UI.menu.contacts',
         icon: 'pi pi-users',
         routerLink: '/contacts'
       },
+      {
+        label: 'UI.menu.change-language',
+        icon: 'pi pi-globe',
+        command: () => {
+          this.changeLang();
+        }
+      }
     ]
     if (this.authService.isAuth()) {
       this.items.push({
-        label: this.translate.instant('UI.menu.logout'),
+        label: 'UI.menu.logout',
         icon: 'pi pi-sign-out',
         command: () => {
           this.authService.logOut();
@@ -38,6 +46,27 @@ export class MenuComponent implements OnInit{
         },
         routerLink: '/shop',
       })
+    }
+
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      const subscription = this.translate.stream(item.label!).subscribe(value => {
+        item.label = value;
+      });
+      this.subscriptions.push(subscription);
+    }
+  }
+
+  changeLang() {
+    const currentLang = this.translate.currentLang;
+    const oppositeLang = currentLang === 'en' ? 'ru' : 'en';
+    this.translate.use(oppositeLang);
+    localStorage.setItem('lang', oppositeLang)
+  }
+
+  ngOnDestroy() {
+    for (let subscription of this.subscriptions) {
+      subscription.unsubscribe();
     }
   }
 }
